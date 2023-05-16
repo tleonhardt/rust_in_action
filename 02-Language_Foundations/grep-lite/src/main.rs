a5015d7169fa3d1dd0cef0888073bbc7166d3c3c
Todd Leonhardt <todd.leonhardt@gmail.com>
2023-05-16 06:48:06 -0400

    grep-lite now supports reading from either stdin or a file

--- 02-Language_Foundations/grep-lite/src/main.rs
+++ 02-Language_Foundations/grep-lite/src/main.rs
@@ -1,8 +1,19 @@
 use clap::{Arg, Command};
 use regex::Regex;
 use std::fs::File;
+use std::io;
 use std::io::prelude::*;
 use std::io::BufReader;
+
+fn process_lines<T: BufRead + Sized>(reader: T, re: Regex) {
+    for line_ in reader.lines() {
+        let line = line_.unwrap();
+        match re.find(&line) {
+            Some(_) => println!("{}", line),
+            None => (),
+        }
+    }
+}
 
 fn main() {
     let matches = Command::new("grep-lite")
@@ -14,22 +25,23 @@ fn main() {
                 .help("The pattern to search for")
                 .required(true),
         )
-        .arg(Arg::new("input").help("File to search").required(true))
+        .arg(Arg::new("input").help("File to search").required(false))
         .get_matches();
 
     let pattern = matches.get_one::<String>("pattern").expect("required");
     let re = Regex::new(pattern).unwrap();
 
-    let input = matches.get_one::<String>("input").expect("required");
-    let f = File::open(input).unwrap();
-    let reader = BufReader::new(f);
+    let default_input = "-".to_string();
 
-    for line_ in reader.lines() {
-        let line = line_.unwrap();
+    let input = matches.get_one::<String>("input").unwrap_or(&default_input);
 
-        match re.find(&line) {
-            Some(_) => println!("{}", line),
-            None => (),
-        }
+    if input == &default_input {
+        let stdin = io::stdin();
+        let reader = stdin.lock();
+        process_lines(reader, re);
+    } else {
+        let f = File::open(input).unwrap();
+        let reader = BufReader::new(f);
+        process_lines(reader, re);
     }
 }
