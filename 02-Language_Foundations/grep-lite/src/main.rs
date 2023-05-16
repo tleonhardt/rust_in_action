ac3137a02dc6398abda578e29579fae95876cc70
Todd Leonhardt <todd.leonhardt@gmail.com>
2023-05-16 06:31:46 -0400

    grep-lite now reads from a file

--- 02-Language_Foundations/grep-lite/src/main.rs
+++ 02-Language_Foundations/grep-lite/src/main.rs
@@ -1,5 +1,8 @@
 use clap::{Arg, Command};
 use regex::Regex;
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::BufReader;
 
 fn main() {
     let matches = Command::new("grep-lite")
@@ -11,17 +14,20 @@ fn main() {
                 .help("The pattern to search for")
                 .required(true),
         )
+        .arg(Arg::new("input").help("File to search").required(true))
         .get_matches();
 
     let pattern = matches.get_one::<String>("pattern").expect("required");
     let re = Regex::new(pattern).unwrap();
 
-    let quote = "Every face, every shop, bedroom window, public-house, and
-    dark square is a picture feverishly turned--in search of what?
-    It is the same with books. What do we seek through millions of pages?";
+    let input = matches.get_one::<String>("input").expect("required");
+    let f = File::open(input).unwrap();
+    let reader = BufReader::new(f);
 
-    for line in quote.lines() {
-        match re.find(line) {
+    for line_ in reader.lines() {
+        let line = line_.unwrap();
+
+        match re.find(&line) {
             Some(_) => println!("{}", line),
             None => (),
         }
