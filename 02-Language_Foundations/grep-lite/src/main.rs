8aa37d7f7510a34aa5924b10cbe642aa3512d1d7
Todd Leonhardt <todd.leonhardt@gmail.com>
2023-05-16 06:05:22 -0400

    Added use of clap for command-line argument parsing

--- 02-Language_Foundations/grep-lite/src/main.rs
+++ 02-Language_Foundations/grep-lite/src/main.rs
@@ -1,16 +1,27 @@
+use clap::{Arg, Command};
 use regex::Regex;
 
 fn main() {
-    let re = Regex::new("picture").unwrap();
+    let matches = Command::new("grep-lite")
+        .version("0.1")
+        .author("Todd Leonhardt")
+        .about("searches for patterns")
+        .arg(
+            Arg::new("pattern")
+                .help("The pattern to search for")
+                .required(true),
+        )
+        .get_matches();
+
+    let pattern = matches.get_one::<String>("pattern").expect("required");
+    let re = Regex::new(pattern).unwrap();
 
     let quote = "Every face, every shop, bedroom window, public-house, and
     dark square is a picture feverishly turned--in search of what?
     It is the same with books. What do we seek through millions of pages?";
 
     for line in quote.lines() {
-        let contains_substring = re.find(line);
-
-        match contains_substring {
+        match re.find(line) {
             Some(_) => println!("{}", line),
             None => (),
         }
